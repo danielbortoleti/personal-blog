09e389b0cdcc74de822ad4dc42f8dc62c9c31382
Daniel Melo <daniel.melo@hsl.org.br>
2024-06-15 16:53:09 -0300

    style: refine styling for home and post pages with basic Lorem Ipsum content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
 export default function Home() {
-  return <main> Home</main>;
+  return <main className="text-center pt-32 px-5">
+    <h1 className="text-4xl md:text-5xl font-bold mb-5">Welcome to my blog.</h1>
+    <p className="max-w-[750px] mx-auto leading-8">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti id corrupti quibusdam neque qui, molestiae eligendi, provident, ab beatae corporis laborum laboriosam eaque facilis. Exercitationem illum libero quisquam aut necessitatibus!</p>
+  </main>;
 }
